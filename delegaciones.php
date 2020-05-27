1ecf8df654e5fb3693bce07e2b7c26e8d9b3dd95
Edgar552 <37153459+Edgar552@users.noreply.github.com>
2020-05-27 09:26:52 -0500

    Update
    a

--- delegaciones.php
+++ delegaciones.php
@@ -1,2 +1,231 @@
+<?php include("header.php");?>
+
 <section class="main-container">
 		<div class="row">
+	
+		<br>
+		<div class="col-md-8">
+		<div class="row">
+
+		<div>
+      <div class="panel-title">
+    <h4>San Luis Potosi</h4></div>
+    <br>
+       <article>
+      <p> Ignacio López Rayón #450, Centro Historico C.P. 78000,  Tel. (444) 812 05 78</p>
+     	  <br>
+     
+       </article>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d865.3156090176824!2d-100.97570336358648!3d22.147657209148903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842aa20138e2b753%3A0xfb14f58dad2e50db!2sComisi%C3%B3n+Ejecutiva+Estatal+de+Atenci%C3%B3n+a+V%C3%ADctimas+de+San+Luis+Potos%C3%AD!5e0!3m2!1ses-419!2smx!4v1515180556732" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+  </div>
+    <br>
+    <br>
+
+  <div>
+    <div class="panel-title">
+    <h4>Matehuala</h4></div>
+    <br>
+    <article>
+      <p> Carretera Matehuala – La Paz km.1, (Campo Betania) C.P. 78724, Tel. (488) 887 70 24 </p>
+            <p> <i class="fa fa-envelope" aria-hidden="true"></i>upc.matehuala@ceeavslp.gob.mx</p>
+          	<br>
+    </article>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2172.816577950503!2d-100.6517794767039!3d23.667826565256046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8680a99259744655%3A0x373519717f89ac02!2sCarretera%2C+Matehuala%2C+S.L.P.!5e0!3m2!1ses!2smx!4v1510708891384" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+      <br>
+          <br>
+  <div>
+    <div class="panel-title">
+    <h4>Ciudad Valles</h4></div>
+    <br>
+        <article>
+      <p>Calle Galeana #435, Ciudad Valles C.P. 79050, Tel. (481) 381 03 68</p>
+        
+          	<p> <i class="fa fa-envelope" aria-hidden="true"></i> upc.cdvalles@ceeavslp.gob.mx</p>
+          	<br>
+    </article>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1099.9369746422112!2d-99.01376324040315!3d21.983234568387385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d672a9e5d30da5%3A0x8f7ad69fa538c2fc!2sGaleana+431%2C+Zona+Centro%2C+79000+Cd+Valles%2C+S.L.P.!5e0!3m2!1ses!2smx!4v1536077785521" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+      <br>
+          <br>
+  <div>
+    <div class="panel-title">
+    <h4>Tamazunchale</h4></div>
+    <br>
+      <article>
+      <p>Av. 20 de noviembre S/N, Esquina con calle 12 de octubre (Plaza Peri), Barrio del Carmen C.P. 79600</p>
+          	<p> <i class="fa fa-envelope" aria-hidden="true"></i> upc.tamazunchale@ceeavslp.gob.mx</p>
+          	<br>
+    </article>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d464.7720316529958!2d-98.79495186210224!3d21.264489721388493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d6ea8a35210d5b%3A0x7f63fb318bf2c203!2sPlaza+Peri!5e0!3m2!1ses!2smx!4v1510709307553" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+   
+  </div>
+   <br>
+        <br>
+    <div>
+      <div class="panel-title">
+    <h4>Rioverde</h4></div>
+    <br>
+          <article>
+      <p>Jorge Ferretiz S/N-Edificio, Subprocuraduría Regional-Zona media, 3er piso Tel. (487) 100 90 63
+      </p>
+          	<p> <i class="fa fa-envelope" aria-hidden="true"></i> upc.rioverde@ceeavslp.gob.mx</p>
+          	<br>
+    </article>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3703.3287894053906!2d-99.95373983543094!3d21.926837935518783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x94f348efa72a3a2d!2sCEPRERESO+No.+2!5e0!3m2!1ses!2smx!4v1515780031007" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+
+  <div>
+    <br>
+      <div class="panel-title">
+    <h4>Cárdenas</h4>
+      </div>
+         <br>
+      <article>
+        <p>Alváro Obregón N° 99 Col. Centro CP:78380 Tel: (487) 873 10 82
+        <br>
+        </p>
+        <br>
+    </article>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d924.8170207072169!2d-99.64251597081865!3d22.0010367990925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d5967d86a07607%3A0x1f40af43e7a079bb!2s%C3%81lvaro+Obreg%C3%B3n+99%2C+Barrio+del+Carmen%2C+79382+C%C3%A1rdenas%2C+S.L.P.!5e0!3m2!1ses!2smx!4v1515778501828" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+  </div>
+    <br>
+    <div>
+      <div class="panel-title">
+    <h4>Ciudad del Maíz</h4>
+      </div>
+         <br>
+      <article>
+        <p>Boulevard Miguel Barragán S/N Zona Centro CP: 79326
+        <br>
+        </p>
+        <br>
+    </article>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7377.389387716606!2d-99.60772408509787!3d22.402864199633807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842ab19a266d68cd%3A0x9668b32513957d7e!2sCuidad+Del+Maiz!5e0!3m2!1ses!2smx!4v1515778838037" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+  <br>
+  <div>
+      <div class="panel-title">
+    <h4>Tancanhuitz</h4>
+      </div>
+         <br>
+      <article>
+        <p>Fco. I. Madero N° 29 Esq. Lauro Muñoz int. 3 Zona Centro CP: 79800 Tel: (482) 367 10 84
+        <br>
+        </p>
+          	<p> <i class="fa fa-envelope" aria-hidden="true"></i> upc.tancanhuitz@ceeavslp.gob.mx</p>
+          	<br>
+    </article>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d788.918913266619!2d-98.9669655461415!3d21.599690875531174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d689cce185ca8f%3A0x174d7b6e63b5c0de!2sJURISDICCION+SANITARIA+VII+TANCANHUITZ!5e0!3m2!1ses!2smx!4v1515779086854" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+    <br>
+    <div>
+      <div class="panel-title">
+    <h4>Cerritos</h4>
+      </div>
+         <br>
+      <article>
+        <p>Alberto Carrera Torres N°75 Col. Gpe. Tel: (486) 863 27 32
+        <br>
+        </p>
+        <br>
+    </article>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d541.6657843201012!2d-100.28224557349841!3d22.41863737691566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x867ff33d8667ae95%3A0x79b84078aed34ff2!2sAlberto+Carrera+Torres+75%2C+Guadalupe%2C+79445+Cerritos%2C+S.L.P.!5e0!3m2!1ses!2smx!4v1515779378385" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+    <br>
+    <div>
+      <div class="panel-title">
+    <h4>Venado</h4>
+      </div>
+         <br>
+      <article>
+        <p>Porfirio Díaz S/N Zona Centro Tel (486) 854 67 75
+        <br>
+        </p>
+        <br>
+    </article>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d829.502790146971!2d-101.09510847718582!3d22.93247628126265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x868059181fc65181%3A0xac0e948dd74cdbc6!2sPorfirio+D%C3%ADaz%2C+Venado%2C+S.L.P.!5e0!3m2!1ses!2smx!4v1515779466820" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+    <br>
+    <div>
+      <div class="panel-title">
+    <h4>Salinas</h4>
+      </div>
+         <br>
+      <article>
+        <p>Allende N° 36 Col. Centro CP:78600 Tel: (496) 963 01 69
+        <br>
+        </p>
+        <br>
+    </article>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d920.6645243611542!2d-101.71719317081778!3d22.62923929907215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8681958f8af3a8b1%3A0x6bc0eae1baf3160c!2sAllende+36%2C+2+de+Abril%2C+Centro%2C+Salinas+de+Hidalgo%2C+S.L.P.!5e0!3m2!1ses!2smx!4v1515780311940" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+  <br>
+    <div>
+      <div class="panel-title">
+    <h4>Santa María del Río</h4>
+      </div>
+         <br>
+      <article>
+        <p>Primo Feliciano Velázquez N° 140 Col. Lindavista (Centro Integral de Justicia)
+        <br>
+        </p>
+        <br>
+    </article>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3704.324617761193!2d-100.73428720614478!3d21.80638511703577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842ac94873bd505b%3A0x105190b46ec4a60c!2sCalle+Primo+Feliciano+Vel%C3%A1zquez+Rodr%C3%ADguez%2C+79560+Santa+Mar%C3%ADa+del+R%C3%ADo%2C+S.L.P.!5e0!3m2!1ses!2smx!4v1515780746852" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+          <br>
+          <br>
+
+		</div>
+<!--TERMINA SLIDE ENLACES DE INTERES-->
+
+		</div>
+ <?php include("aside.php");?>
+
+		</div>
+	
+
+</div>
+
+
+
+</section>
+
+<?php include("footer.php");?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../js/jquery.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+
+<script>
+	
+	// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+</script>
+
+
+</body>
+</html>
